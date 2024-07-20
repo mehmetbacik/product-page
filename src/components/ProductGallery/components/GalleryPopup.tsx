07c7cb5dc9c7f75db87098e128a9fc1adfdae761
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-07-20 22:47:01 +0300

    Update
    Updated for GalleryPopup styles.

--- src/components/ProductGallery/components/GalleryPopup.tsx
+++ src/components/ProductGallery/components/GalleryPopup.tsx
@@ -24,43 +24,40 @@ const GalleryPopup: React.FC<GalleryPopupProps> = ({
 }) => {
   return (
     <motion.div
-      className="fixed inset-0 bg-black bg-opacity-75 z-50 flex justify-center items-center"
+      className="gallery-popup bg-black bg-opacity-75"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
       <motion.div
-        className="relative bg-white p-4 rounded max-w-2xl w-full mx-auto"
+        className="relative max-w-lg w-full mx-auto"
         initial={{ scale: 0.9 }}
         animate={{ scale: 1 }}
         exit={{ scale: 0.9 }}
       >
-        <div className="relative">
+        <div className="gallery-content">
           <img
             src={images[selectedImage]}
             alt={`Image ${selectedImage}`}
-            className="max-w-full max-h-full"
+            className="product-image max-w-full max-h-full"
           />
-          <button
-            className="absolute top-4 right-4 text-white text-2xl"
-            onClick={onClose}
-          >
-            <img src={CloseIcon} alt="Close" className="w-6 h-6" />
+          <button className="icon-close" onClick={onClose}>
+            <img src={CloseIcon} alt="Close" />
           </button>
           <button
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white text-2xl"
+            className="icon-prev transform -translate-y-1/2"
             onClick={onPrev}
           >
-            <img src={PrevIcon} alt="Previous" className="w-6 h-6" />
+            <img src={PrevIcon} alt="Previous" />
           </button>
           <button
-            className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white text-2xl"
+            className="icon-next transform -translate-y-1/2"
             onClick={onNext}
           >
-            <img src={NextIcon} alt="Next" className="w-6 h-6" />
+            <img src={NextIcon} alt="Next" />
           </button>
         </div>
-        <div className="thumbnails grid grid-cols-4 gap-2 mt-4">
+        <div className="thumbnails">
           {images.map((image, index) => (
             <Thumbnail
               key={index}
